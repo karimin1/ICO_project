0124981f920d55335dc4bf2e615caf1df07717a6
karimsk <kariminsaid11@gmail.com>
2025-07-11 12:25:00 +0100

    update12

--- Utils/web3modal.js
+++ Utils/web3modal.js
@@ -1,38 +1,31 @@
-import Web3Modal from 'web3modal'
-import { ethers } from 'ethers';
+import Web3Modal from "web3modal";
+import { ethers } from "ethers";
 
- let web3modal=null;
-let isConnecting=false;
-export const getProviderOrSigner =async()=>{
-
-
-  if(typeof window=="undefined"){
-    throw new Error ('This function can only run in the browser');
+let web3modal = null;
+let isConnecting = false;
+export const getProviderOrSigner = async () => {
+  if (typeof window == "undefined") {
+    throw new Error("This function can only run in the browser");
   }
-  if(!web3modal){
-    web3modal=new Web3Modal({cacheProvider:true});
-    console.log('web3modal',web3modal);
+  if (!web3modal) {
+    web3modal = new Web3Modal({ cacheProvider: true });
+    console.log("web3modal", web3modal);
   }
-  if (isConnecting){
-    console.log('alreading cnnecting');
+  if (isConnecting) {
+    console.log("alreading cnnecting");
     return;
   }
 
-  isConnecting=true;
-  try{
-  const connection=await web3modal.connect();
-  const provider=new ethers.BrowserProvider(connection);
-  const signer=await provider.getSigner();
+  isConnecting = true;
+  try {
+    const connection = await web3modal.connect();
+    const provider = new ethers.BrowserProvider(connection);
+    const signer = await provider.getSigner();
 
-
-   return {provider,signer};
-  
+    return { provider, signer };
+  } catch (error) {
+    console.log("eror", error);
+  } finally {
+    isConnecting = false;
   }
-catch(error){
-  console.log('eror',error);
-}
-finally {
-  isConnecting = false;
-}
-
-}
+};
